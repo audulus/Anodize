0581745f27522cf31b984e56418496094c2ae171
Taylor Holliday <wtholliday@gmail.com>
2025-02-21 12:21:49 -0800

    create demo project

---
+++ Demo/AnodizeDemo/AnodizeDemo/AnodizeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnodizeDemoApp.swift
+//  AnodizeDemo
+//
+//  Created by Taylor Holliday on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AnodizeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

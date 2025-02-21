b15965e3238611ce918ee2705e8fa280ca5d6b86
Taylor Holliday <wtholliday@gmail.com>
2025-02-21 12:45:38 -0800

    add code

--- Demo/AnodizeDemo/AnodizeDemo/AnodizeDemoApp.swift
+++ Demo/AnodizeDemo/AnodizeDemo/AnodizeDemoApp.swift
@@ -6,12 +6,44 @@
 //
 
 import SwiftUI
+import AnodizeUtil
+
+func runMyKernel() {
+
+    do {
+        let device = MTLCreateSystemDefaultDevice()!
+        let kernel = MyCoolKernel(device: device)
+
+        let array = try! MutableGPUArray<Float>(data: [1,2,3])
+
+        let queue = device.makeCommandQueue()!
+        let buf = queue.makeCommandBuffer()!
+
+        try kernel
+            .begin(buf)
+            .buffer(array)
+            .value(bytes: 1)
+            .dispatch(threads: 3, threadsPerThreadgroup: 1)
+            .end()
+
+        buf.commit()
+        buf.waitUntilCompleted()
+
+        print("array: \(array.array)")
+
+    } catch {
+        print("⚠️ error: \(error)")
+    }
+}
 
 @main
 struct AnodizeDemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    runMyKernel()
+                }
         }
     }
 }

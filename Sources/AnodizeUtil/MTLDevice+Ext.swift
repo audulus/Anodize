4fd677dfded4ae6e2d1f020453e398518c0a0848
Taylor Holliday <wtholliday@gmail.com>
2025-02-21 12:34:46 -0800

    Create MTLDevice+Ext.swift

---
+++ Sources/AnodizeUtil/MTLDevice+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  MTLDevice+Ext.swift
+//  Anodize
+//
+//  Created by Taylor Holliday on 2/21/25.
+//
+
+public extension MTLDevice {
+    func makeBuffer<T>(_ array: [T]) -> MTLBuffer? {
+        array.withUnsafeBytes { ptr in
+            makeBuffer(bytes: ptr.baseAddress!, length: ptr.count)
+        }
+    }
+
+    func makeComputePipeline(name: String) -> MTLComputePipelineState {
+        let lib = makeDefaultLibrary()!
+
+        let function = lib.makeFunction(name: name)!
+        let computeDesc = MTLComputePipelineDescriptor()
+        computeDesc.computeFunction = function
+        computeDesc.label = name
+        return try! makeComputePipelineState(descriptor: computeDesc, options: []).0
+    }
+}
